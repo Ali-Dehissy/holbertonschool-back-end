5bb01e07b3e3f88f21eaea8fa9b15de6f6f2b932
Ali-Dehissy <ali1998dehissy@gmail.com>
2022-12-04 23:07:30 +0100

    gather

---
+++ api/0-gather_data_from_an_API.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""Gathering DATA from and api"""
+import requests
+import sys
+
+if __name__ == "__main__":
+    api_url = "https://jsonplaceholder.typicode.com/"
+    user = requests.get(api_url + "users/{}".format(sys.argv[1])).json()
+    todos = requests.get(
+        api_url + "todos", params={"userId": sys.argv[1]}).json()
+
+    completed = []
+    for task in todos:
+        if task.get("completed") is True:
+            completed.append(task.get("title"))
+    print("Employee {} is done with tasks({}/{}):".format(
+        user.get("name"), len(completed), len(todos)))
+    for complete in completed:
