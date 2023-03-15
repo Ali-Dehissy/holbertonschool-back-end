8c9e13ef0aaf1f488f997db36960424077f4afb9
Ali-Dehissy <ali1998dehissy@gmail.com>
2023-03-15 12:18:52 +0100

    module is documented fix

---
+++ api/0-gather_data_from_an_API.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""Returns a TO DO list for a given employe ID"""
+
+import requests
+from sys import argv
+
+if __name__ == "__main__":
+    employee = int(argv[1])
+    user = requests.get(
+        "https://jsonplaceholder.typicode.com/users/{}".format(employee)
+    ).json()
+    TODO = requests.get(
+        "https://jsonplaceholder.typicode.com/todos?userId={}".format(employee)
+    ).json()
+    task_done = []
+    for task in TODO:
+        if task.get("completed") is True:
+            task_done.append(task.get("title"))
+    print(
+        "Employee {} is done with tasks({}/{}):".format(
+            user.get("name"), len(task_done), len(TODO)
+        )
+    )
+    for done in task_done:
+        print("\t {}".format(done))
+
